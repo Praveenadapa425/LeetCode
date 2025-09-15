b8608590aaaa8d098b77ea7a291b06b8f12b29ea
Praveen Adapa <147489706+Praveenadapa425@users.noreply.github.com>
2025-09-15 19:58:06 +0530

    Time: 1 ms (99.3%), Space: 41.9 MB (96.29%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
@@ -2,12 +2,17 @@ class Solution {
     public int canBeTypedWords(String text, String brokenLetters) {
         int count = 0;
         boolean flag = true;
+        boolean[] arr = new  boolean[26];
+        for(char ch : brokenLetters.toCharArray()){
+            arr[ch - 'a'] = true;   // marking broken char
+        }
+
         for(char ch : text.toCharArray()){
             if(ch == ' '){
                 if(flag == true) count++;  
                 flag = true;
                 
-            }else if(flag && brokenLetters.indexOf(ch) != -1){
+            }else if(flag && arr[ch - 'a']){
 
                     flag = false; // if broken char appears 
 
@@ -18,5 +23,6 @@ class Solution {
         }
         if(flag) count++;
         return count;
+        
     }
 }

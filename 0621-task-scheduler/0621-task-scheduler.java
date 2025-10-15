401d1f2e671e811a14145a3ff76cea3315647f75
Praveen Adapa <147489706+Praveenadapa425@users.noreply.github.com>
2025-10-15 21:56:42 +0530

    Time: 2 ms (98.53%), Space: 46.1 MB (32.53%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,26 +1,41 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        
-        int[] freq = new int[26];
-        for (char task : tasks) {
-            freq[task - 'A']++;
+        int count[] =new int[26];
+        for(char C: tasks){
+            count[C -'A']++;
         }
-        
-        int maxFreq = 0;
-        for (int f : freq) {
-            maxFreq = Math.max(maxFreq, f);
+        Arrays.sort(count);
+        int maxC=count[25];
+
+        int freq=0;
+
+        for(int i=25;i>=0;i--){
+            if(count[i]==maxC)freq++;
+            else break;
         }
+        int minC=(maxC-1)*(n+1)+freq;
+
+        return Math.max(minC,tasks.length);
+        // int[] freq = new int[26];
+        // for (char task : tasks) {
+        //     freq[task - 'A']++;
+        // }
         
-        int maxCount = 0;
-        for (int f : freq) {
-            if (f == maxFreq) {
-                maxCount++;
-            }
-        }
+        // int maxFreq = 0;
+        // for (int f : freq) {
+        //     maxFreq = Math.max(maxFreq, f);
+        // }
         
-        int totalTasks = tasks.length;
-        int formulaResult = (maxFreq - 1) * (n + 1) + maxCount;
+        // int maxCount = 0;
+        // for (int f : freq) {
+        //     if (f == maxFreq) {
+        //         maxCount++;
+        //     }
+        // }
         
-        return Math.max(totalTasks, formulaResult);
+        // int totalTasks = tasks.length;
+        // int formulaResult = (maxFreq - 1) * (n + 1) + maxCount;
+        
+        // return Math.max(totalTasks, formulaResult);
     }
 }

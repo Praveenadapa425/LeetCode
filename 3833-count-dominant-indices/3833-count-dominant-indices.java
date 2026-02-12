14eced2a165cc186d420ae261882004af7edb6a2
Praveen Adapa <147489706+Praveenadapa425@users.noreply.github.com>
2026-02-12 11:43:09 +0530

    Time: 1 ms (100%), Space: 46.2 MB (85.13%) - LeetHub

--- 3833-count-dominant-indices/3833-count-dominant-indices.java
+++ 3833-count-dominant-indices/3833-count-dominant-indices.java
@@ -1,22 +1,32 @@
 class Solution {
     public int dominantIndices(int[] nums) {
-        if(nums.length == 1) return 0;
+        int sum = 0;
         int n = nums.length;
-        long sum =0;
-        int count =0;
+        for(int i=0;i<n;i++) sum+= nums[i];
 
-        for(int i=n-1; i>=0;i--){
-           
-            System.out.println(sum + " "+n+" "+i);
-            if(i <n-1){
-                long suffixLen = n - i - 1;
-                double avg = (double) sum/ suffixLen;
-                if(nums[i] > avg){
-                    count++;
-                }
-            }
-            sum+=nums[i];
+        int count =0;
+        for(int i=0;i<n-1;i++){
+            sum = sum -nums[i];
+
+            if(nums[i] > sum /(n-i-1)) count++;
         }
+        // if(nums.length == 1) return 0;
+        // int n = nums.length;
+        // long sum =0;
+        // int count =0;
+
+        // for(int i=n-1; i>=0;i--){
+           
+        //     System.out.println(sum + " "+n+" "+i);
+        //     if(i <n-1){
+        //         long suffixLen = n - i - 1;
+        //         double avg = (double) sum/ suffixLen;
+        //         if(nums[i] > avg){
+        //             count++;
+        //         }
+        //     }
+        //     sum+=nums[i];
+        // }
         return count;
     }
 }

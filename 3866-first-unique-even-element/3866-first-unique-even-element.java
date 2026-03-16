b28618f18983e75fa0775ff8c4d8d109c0817cf5
Praveen Adapa <147489706+Praveenadapa425@users.noreply.github.com>
2026-03-16 21:15:29 +0530

    Time: 1 ms (100%), Space: 46.1 MB (50%) - LeetHub

---
+++ 3866-first-unique-even-element/3866-first-unique-even-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int firstUniqueEven(int[] nums) {
+        int[] count = new int[101];
+        for(int num : nums){
+            count[num]++;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] % 2 == 0 && count[nums[i]] == 1){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}

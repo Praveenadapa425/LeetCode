35bacdbcfaaeec81a9a11865ed54561d26ddfbfd
Praveen Adapa <147489706+Praveenadapa425@users.noreply.github.com>
2026-05-26 22:07:07 +0530

    Time: 1 ms (100%), Space: 43.4 MB (89.9%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -1,20 +1,18 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-        boolean[] lowerCase = new boolean[26];
-        boolean[] upperCase = new boolean[26];
+        boolean[] lower = new boolean[26];
+        boolean[] upper = new boolean[26];
         for(char ch : word.toCharArray()){
-            int t = ch;
-            if(t>=65 && t<=90){
-                upperCase[t-65] = true;
+            if(Character.isLowerCase(ch)){
+                lower[ch - 'a'] = true;
             }else{
-                lowerCase[t-97] = true; 
+                upper[ch - 'A'] = true;
             }
-            //  System.out.println(ch +" "+t);
         }
 
         int special  =0;
         for(int i=0;i<26;i++){
-            if(lowerCase[i] && upperCase[i]) special++;
+            if(lower[i] && upper[i]) special++;
         }
         return special;
     }

c6c49411da7a52cd43f5897b0f56d1c6223df922
Praveen Adapa <147489706+Praveenadapa425@users.noreply.github.com>
2026-06-04 14:10:11 +0530

    Time: 0 ms (100%), Space: 42.9 MB (65.06%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -10,18 +10,27 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode temp = dummyHead;
-        temp.next = head;
-        ListNode t1 = head;
-        while(temp.next != null && t1.next != null){
-            temp.next = t1.next;
-            t1.next = t1.next.next;
-            temp.next.next = t1;
-
-            temp = temp.next.next;
-            t1 = temp.next;
+        if(head== null || head.next == null){
+            return head;
         }
-       return dummyHead.next;
+        ListNode second = head.next;
+        head.next = swapPairs(second.next);
+        second.next = head;
+        return second;
+    //     ListNode dummyHead = new ListNode(0);
+    //     ListNode temp = dummyHead;
+    //     temp.next = head;
+    //     ListNode t1 = head;
+    //     //  0    1   2  3
+    //     //  temp t1
+    //     while(temp.next != null && t1.next != null){
+    //         temp.next = t1.next; //0 -->2
+    //         t1.next = t1.next.next;//1-->3
+    //         temp.next.next = t1;// 2 -->1
+
+    //         temp = temp.next.next;
+    //         t1 = temp.next;
+    //     }
+    //    return dummyHead.next;
     }
 }

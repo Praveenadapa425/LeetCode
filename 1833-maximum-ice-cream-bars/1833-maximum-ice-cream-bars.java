7cd93aca15922175931d6df50912fd562ac5127c
Praveen Adapa <147489706+Praveenadapa425@users.noreply.github.com>
2026-06-24 11:52:04 +0530

    Time: 8 ms (76.3%), Space: 81.6 MB (96.49%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,12 +1,18 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
+        int[] freq = new int[100001];
+        for(int cost : costs){
+            freq[cost]++;
+        }
         int bars = 0;
-        for(int cost:costs){
-            if(coins >= cost){
-                coins -= cost;
-                bars++;
-            }else{
+        for(int cost=1;cost<freq.length;cost++){
+            int canBuy = Math.min(freq[cost], coins/cost);
+
+            bars += canBuy;
+
+            coins -= canBuy * cost;
+
+            if (coins < cost) {
                 break;
             }
         }

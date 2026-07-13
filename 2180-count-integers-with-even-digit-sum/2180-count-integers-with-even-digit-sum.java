6a82b0f992a2c0194df4b24cae359fedaa1ee830
Praveen Adapa <147489706+Praveenadapa425@users.noreply.github.com>
2026-07-13 10:34:04 +0530

    Time: 2 ms (59.82%), Space: 42 MB (56.58%) - LeetHub

---
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countEven(int num) {
+        int ans = 0;
+        for(int i=1;i<=num ; i++){
+            
+             int x = i,sum = 0;
+             while(x > 0){
+                sum += x %10;
+                x /= 10;
+             }
+             if(sum % 2 == 0) ans++;
+        }
+        return ans;
+    }
+}
